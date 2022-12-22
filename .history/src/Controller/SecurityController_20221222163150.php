2feb3aba140be953af34b015eb29a5538ce11ac6
AGESILAS Ludovic <ldc45@outlook.fr>
2022-12-22 16:40:22 +0100

    ✨Paramétrage du Firewall

---
+++ .history/src/Controller/SecurityController_20221222163150.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connexion', name: 'security.login', methods:['GET', 'POST'])]
+    public function login(): Response
+    {
+        return $this->render('pages/security/login.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'security.logout', methods:['GET', 'POST'])]
+    public function logout(): Response
+    {
+        return $this->render('pages/security/login.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+
+}
